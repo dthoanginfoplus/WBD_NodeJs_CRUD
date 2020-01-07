5098b1bb529dcd91db980e0bf3c3702635e2a58a
dthoanginfoplus <hoangdt@infoplusvn.com>
2020-01-07 15:41:22 +0700

    commit

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 var user = function(object) {
     console.log(object);
     this.name =  object.name;
-    this.username = object.username;
     this.password = object.password;
     this.email = object.email;
 }
